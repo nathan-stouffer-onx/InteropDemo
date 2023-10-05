dc33572588d073341f3c8618ef9737003e87fd46
nathan-stouffer-onx <nathan.stouffer@onxmaps.com>
2023-10-05 11:45:18 -0600

    Initial Commit

---
+++ InteropDemo/InteropDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteropDemoApp.swift
+//  InteropDemo
+//
+//  Created by Nathan Stouffer on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct InteropDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
