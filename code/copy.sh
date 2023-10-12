550128378c6be43bae90be9fdf6f6d44a28bac04
nathan-stouffer-onx <nathan.stouffer@onxmaps.com>
2023-10-12 10:59:55 -0600

    utilize libs per architecture

--- code/copy.sh
+++ code/copy.sh
@@ -2,5 +2,10 @@
 
 ./build.sh
 
-cp -r build/artifacts/x86_64-ios/include/* ../CxxTest/include/
-cp -r build/artifacts/x86_64-ios/lib/libonyx.a ../CxxTest/
+cp -rv build/artifacts/x86_64-ios/include/* ../CxxTest/include/
+mkdir ../CxxTest/x86_64
+cp -rv build/artifacts/x86_64-ios/lib ../CxxTest/x86_64/
+
+cp -rv build/artifacts/arm64-ios/include/* ../CxxTest/include/
+mkdir ../CxxTest/arm64
+cp -rv build/artifacts/arm64-ios/lib ../CxxTest/arm64/
