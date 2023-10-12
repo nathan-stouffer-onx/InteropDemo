c7e7c87abbe3e58fd5976a0c30dc1b71bec23964
nathan-stouffer-onx <nathan.stouffer@onxmaps.com>
2023-10-12 15:01:42 -0600

    get app building on device

--- code/build.sh
+++ code/build.sh
@@ -3,10 +3,10 @@
 export DEVICE_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk
 export SIMULATOR_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk
 
-# configure and build for arm64 ios
-cmake --fresh -S . -B build -DTARGET_ARCHITECTURE=arm64-ios -DCMAKE_CXX_COMPILER_TARGET=arm64-ios -DCMAKE_OSX_SYSROOT=${DEVICE_SYSROOT}
+# configure and build for arm64 ios device
+cmake --fresh -S . -B build -DTARGET_DIRECTORY=device/arm64-ios -DCMAKE_CXX_COMPILER_TARGET=arm64-ios -DCMAKE_OSX_SYSROOT=${DEVICE_SYSROOT}
 make -C build install
 
-# configure and build for x64 ios
-cmake --fresh -S . -B build -DTARGET_ARCHITECTURE=x86_64-ios -DCMAKE_CXX_COMPILER_TARGET=x86_64-ios -DCMAKE_OSX_SYSROOT=${SIMULATOR_SYSROOT}
+# configure and build for x64 ios simulator
+cmake --fresh -S . -B build -DTARGET_DIRECTORY=simulator/x86_64-ios -DCMAKE_CXX_COMPILER_TARGET=x86_64-ios -DCMAKE_OSX_SYSROOT=${SIMULATOR_SYSROOT}
 make -C build install
--- code/copy.sh
+++ code/copy.sh
@@ -2,10 +2,14 @@
 
 ./build.sh
 
-cp -rv build/artifacts/x86_64-ios/include/* ../CxxTest/include/
-mkdir ../CxxTest/x86_64
-cp -rv build/artifacts/x86_64-ios/lib ../CxxTest/x86_64/
+cp -rv build/installed/device/arm64-ios/include/* ../CxxTest/include/
+mkdir -p ../CxxTest/device/arm64
+cp -rv build/installed/device/arm64-ios/lib ../CxxTest/device/arm64/
 
-cp -rv build/artifacts/arm64-ios/include/* ../CxxTest/include/
-mkdir ../CxxTest/arm64
-cp -rv build/artifacts/arm64-ios/lib ../CxxTest/arm64/
+cp -rv build/installed/simulator/x86_64-ios/include/* ../CxxTest/include/
+mkdir -p ../CxxTest/simulator/x86_64
+cp -rv build/installed/simulator/x86_64-ios/lib ../CxxTest/simulator/x86_64/
+
+# cp -rv build/installed/simulator/arm64-ios/include/* ../CxxTest/include/
+# mkdir -p ../CxxTest/simulator/arm64
+# cp -rv build/installed/simulator/arm64-ios/lib ../CxxTest/simulator/arm64/
