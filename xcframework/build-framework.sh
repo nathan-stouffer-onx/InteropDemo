883ca68e90a066ad426931b97afe67c0c26f49f7
nathan-stouffer-onx <86129622+nathan-stouffer-onx@users.noreply.github.com>
2023-10-13 16:02:18 -0600

    xcframework (#3)
    * update libs
    * produce some sort of xcframework
    * rename xcframework
    * convert static libs into xcframework
    * remove unnecessary include

---
+++ xcframework/build-framework.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+rm -rf onyx.xcframework
+
+cd ../code
+./build.sh
+cd ${OLDPWD}
+
+PREFIX=../code/build/installed
+
+cp onyx.hpp ${PREFIX}/device/arm64-ios/include/
+cp module.modulemap ${PREFIX}/device/arm64-ios/include/
+
+cp onyx.hpp ${PREFIX}/simulator/x86_64-ios/include/
+cp module.modulemap ${PREFIX}/simulator/x86_64-ios/include/
+
+xcodebuild -create-xcframework \
+    -library ${PREFIX}/device/arm64-ios/lib/libonyx.a -headers ${PREFIX}/device/arm64-ios/include \
+    -library ${PREFIX}/simulator/x86_64-ios/lib/libonyx.a -headers ${PREFIX}/simulator/x86_64-ios/include \
+    -output onyx.xcframework
---
+++ xcframework/onyx.xcframework/ios-x86_64-simulator/Headers/CxxTest.hpp
@@ -0,0 +1,13 @@
+//
+//  CxxTest.hpp
+//  CxxTest
+//
+//  Created by Nathan Stouffer on 10/5/23.
+//
+
+#ifndef CxxTest_hpp
+#define CxxTest_hpp
+
+#include "API.h"
+
+#endif /* CxxTest_hpp */
