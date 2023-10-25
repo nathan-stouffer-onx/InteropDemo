91818d94b97fffcba8b1e25ecc61c40816627896
nathan-stouffer-onx <nathan.stouffer@onxmaps.com>
2023-10-25 16:16:44 -0600

    set bundle prefix

--- InteropDemo/ContentView.swift
+++ InteropDemo/ContentView.swift
@@ -23,12 +23,30 @@ struct ContentView: View {
     var device: MTLDevice! = MTLCreateSystemDefaultDevice()
     var metalLayer: CAMetalLayer! = window()
     
+    func files() {
+        let fm = FileManager.default
+        do
+        {
+            let items = try fm.subpathsOfDirectory(atPath: NSHomeDirectory())
+            for item in items {
+                print("Found \(item)")
+            }
+        } catch {
+            print("here")
+        }
+    }
+    
     func initialize() {
         metalLayer.device = device
         let unsafeMetalLayer = UnsafeMutableRawPointer(Unmanaged.passRetained(metalLayer).toOpaque())
         let unsafeMetalDevice = UnsafeMutableRawPointer(Unmanaged.passRetained(device).toOpaque())
 
-        onyx.initialize(90, 90, unsafeMetalLayer, unsafeMetalDevice)
+        guard let bundle = Bundle(path: Bundle.main.bundlePath + "/TestOnyxPackage_Assets.bundle")?.bundlePath else {
+            print("Failed to find assets path")
+            return
+        }
+                
+        onyx.initialize(90, 90, unsafeMetalLayer, unsafeMetalDevice, std.string(bundle + "/"))
     }
     func shutdown() {
         onyx.shutdown()
@@ -39,6 +57,9 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+            Button(action: files, label: {
+                Text("files")
+            })
             Button(action: initialize, label: {
                 Text("initiliaze")
             })
