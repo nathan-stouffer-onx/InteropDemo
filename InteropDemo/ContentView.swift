cfb4417fb89116e2c053099719f36285b19bf5de
nathan-stouffer-onx <86129622+nathan-stouffer-onx@users.noreply.github.com>
2023-12-01 15:35:48 -0700

    Show map (#7)
    * update to latest package
    * update gitignore
    * add swiftpm files

--- InteropDemo/ContentView.swift
+++ InteropDemo/ContentView.swift
@@ -4,7 +4,7 @@ import Metal
 
 struct ContentView: View {
     var body: some View {
-        OnyxView()
+        MapView()
     }
 }
 
--- InteropDemo/InteropDemo.swift
+++ InteropDemo/InteropDemo.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 @main
-struct InteropDemoApp: App {
+struct ConsumeLocalSwiftPackageApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
